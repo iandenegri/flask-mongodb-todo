55c65965ba2ba291e9682cb62ee7f5417c19a3eb
Ian Denegri <iandenegri@hotmail.com>
2021-12-09 18:29:04 -0500

    Get collection working. Logic for making doc

--- todo_app/main/routes.py
+++ todo_app/main/routes.py
@@ -2,14 +2,30 @@
 from flask import Blueprint, render_template, redirect, url_for, request
 
 # Local
+from ..extensions import mongo
 
 main = Blueprint("main", __name__)
 
 @main.route('/')
 def index():
-    return render_template("index.html")
+    todos_collection = mongo.db.todo
+    todo_items = todos_collection.find()
+    return render_template("index.html", todos=todo_items)
 
 @main.route('/add_todo', methods=['POST'])
 def add_todo():
+    todos_collection = mongo.db.todo
+    print(todos_collection)
     todo_item = request.form.get('new-todo')
+    if todo_item:
+        todos_collection.insert_one(
+            {
+                'text': todo_item,
+                'complete': False
+            }
+        )
     return redirect(url_for('main.index'))
+
+@main.route('/finish_todo', methods=['POST'])
+def complete_todo():
+    pass
